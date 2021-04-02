9e5e3980033cf8823f0dc6e6ace73169707d3c2a
Tejas A. Sheth <tejas1004sheth@gmail.com>
2021-04-03 00:34:44 +0530

    Dt.03/04/2021

--- TicTacToe/chance.py
+++ TicTacToe/chance.py
@@ -4,10 +4,10 @@ def chance(window,p_val,counter,terminate,player1,player2,keypress_count,box_num
     for box_num_value in range(1,10):
         if box_num==box_num_value:
             if counter%2!=0:
-                counter=counter+1
+                counter+=1
                 player1.append(box_num)
                 return 'O',counter
             if counter%2==0:
-                counter=counter+1
+                counter+=1
                 player2.append(box_num)
                 return 'X',counter
