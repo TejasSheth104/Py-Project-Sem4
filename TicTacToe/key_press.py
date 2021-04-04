50def9138a2e0c11bc78f0f729ffd3b6ff797d41
Tejas A. Sheth <tejas1004sheth@gmail.com>
2021-04-04 14:14:07 +0530

    Dt.04.04.2021

--- TicTacToe/key_press.py
+++ TicTacToe/key_press.py
@@ -16,18 +16,17 @@ def limit_keypress(window,p_val,counter,terminate,player1,player2,keypress_count
 def key_press(window,p_val,terminate,box_num,val,place_value):
     global counter,keypress_count,player1,player2
     if limit_keypress(window,p_val,counter,terminate,player1,player2,keypress_count,box_num):
-        box_val=chance.chance(window,p_val,terminate,player1,player2,keypress_count,box_num)
-        p_val=box_val+val
+        p_val=chance.chance(window,p_val,terminate,player1,player2,keypress_count,box_num)+val
         place_value.set(p_val)
         if win_con.win_cond(window,p_val,counter,terminate,player1,player2,keypress_count):
             msg=askquestion(title="TRY AGAIN", message="WANT TO PLAY AGAIN?")
             if msg=='yes':
-                draw_grid.draw_grid(window,p_val,counter,terminate,player1,player2,keypress_count)
                 terminate=False
                 counter=0
                 p_val=""
                 player1,player2=list(),list()
                 keypress_count={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+                draw_grid.draw_grid(window,p_val,counter,terminate,player1,player2,keypress_count)
             if msg=='no':
                 showinfo("EXIT","THANK YOU. !!!")
                 window.destroy()
