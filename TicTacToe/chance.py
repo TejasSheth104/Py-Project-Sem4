6e3da192714280e58e57b21a9e85890e9871bcb7
Tejas A. Sheth <tejas1004sheth@gmail.com>
2021-04-05 17:29:52 +0530

    Dt.05/04/2021

--- TicTacToe/chance.py
+++ TicTacToe/chance.py
@@ -14,13 +14,13 @@ def chance(player1,player2,box_num,button_play1,button_play2):
             if counter%2==0:
                 counter+=1
                 player1.append(box_num)
-                button_play1.config(state=DISABLED)
-                button_play2.config(state=ACTIVE)
+                button_play1.config(state=DISABLED)         # disable button 1
+                button_play2.config(state=ACTIVE)           # enable button 2
                 return 'X'
 
             elif counter%2!=0:
                 counter+=1
                 player2.append(box_num)
-                button_play2.config(state=DISABLED)
-                button_play1.config(state=ACTIVE)
+                button_play2.config(state=DISABLED)         # disable button 2
+                button_play1.config(state=ACTIVE)           # enable button 1
                 return 'O'
